8b8133f08065e34b1d9cd35979fdbb316244dc10
MachuaninEzequiel <ezequieladrianmachuanin@gmail.com>
2026-05-07 15:50:43 -0300

    cambio en el mensaje

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mensaje = "Versión original del repositorio upstream";
 
-const   mensaje2 = "Version con cambios";
+const   mensaje2 = "Version con mas cambios";
 
 document.getElementById("mensaje").textContent = mensaje2;
